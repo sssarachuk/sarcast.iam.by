2eb1355eea38e550ec2ce0f557c97d3f0649dfed
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-09-23 21:08:11 +0300

    Page structure is added

--- protected/views/quiz/index.php
+++ protected/views/quiz/index.php
@@ -1,3 +1,12 @@
-<section>
-<span>This is quiz section</span>
-</section>
+<main class="quiz">
+    <article class="section section-variant-1 bg-white text-center">
+        <h1>Название квиза</h1>
+        <div>
+            <sction>
+                <h2>Текст вопроса</h2>
+                <div>Варианты ответов</div>
+            </sction>
+        </div>
+        <footer>Кнопки next-prev</footer>
+    </article>
+</main>
