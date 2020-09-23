32f1932027ec1f72c6ea9b480fa7cfb24d483e70
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-09-23 22:22:32 +0300

    Enchance quiz structure UI

--- protected/views/quiz/index.php
+++ protected/views/quiz/index.php
@@ -1,12 +1,21 @@
 <main class="quiz">
     <article class="section section-variant-1 bg-white text-center">
         <h1>Название квиза</h1>
-        <div>
+        <div class="quiz quiz-body">
             <sction>
                 <h2>Текст вопроса</h2>
                 <div>Варианты ответов</div>
             </sction>
         </div>
-        <footer>Кнопки next-prev</footer>
+        <footer class="quiz quiz-footer">
+            <button class="button button-primary button-ujarak button-grey">
+                <span class="icon mdi mdi-arrow-left"></span>
+                Предыдущий
+            </button>
+            <button class="button button-primary button-ujarak button-pink button-inline">
+                Следующий
+                <span class="icon mdi mdi-arrow-right"></span>
+            </button>
+        </footer>
     </article>
 </main>
