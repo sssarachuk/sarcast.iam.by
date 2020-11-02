d1c56670ec1be798b9c24bb7050481c0e5bbdf90
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-02 19:09:04 +0300

    Add layout for quiz title

--- protected/components/widgets/Quiz/views/quiz-title.php
+++ protected/components/widgets/Quiz/views/quiz-title.php
@@ -1,13 +1,16 @@
-<section class="section section-variant-1 bg-white text-center">
+<section class="section section-variant-1 bg-white">
     <div class="container">
         <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
+            <div class="col-xs-12 col-sm-6 col-md-6">
                 <img src="<?=$model->previewImg;?>"></img>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">
+            <div class="col-xs-12 col-sm-6 col-md-6">
                 <h3><?=$model->title;?></h3>
                 <div><?=$model->description;?></div>
-                <div><?=$model->startButtonTitle;?></div>
+                <button class="button button-primary button-ujarak button-pink">
+                    <span class="icon mdi mdi-checkbox-marked-circle-outline"></span>
+                    <?=$model->startButtonTitle;?>
+                </button>
             </div>
         </div>
     </div>
