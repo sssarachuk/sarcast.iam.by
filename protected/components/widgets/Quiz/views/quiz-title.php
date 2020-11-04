7a7e096b17bd0e638d465eef24ab7a311d4ce6dd
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-04 15:52:17 +0300

    Коррекция стилей страницы-title

--- protected/components/widgets/Quiz/views/quiz-title.php
+++ protected/components/widgets/Quiz/views/quiz-title.php
@@ -1,10 +1,11 @@
-<section class="section section-variant-1 bg-white">
+<section class="section section-variant-1 bg-white quiz-title">
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-6">
                 <img src="<?=$model->previewImg;?>"></img>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="quiz-title__divider"></div>
                 <h3><?=$model->title;?></h3>
                 <div><?=$model->description;?></div>
                 <button onClick='location.href="?index=0"' class="button button-primary button-ujarak button-pink">
