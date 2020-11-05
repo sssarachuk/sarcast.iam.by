99c13ac86ed18be4b1302099a14c177431b3ba81
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-05 18:02:49 +0300

    Fix style for mobile version

--- protected/components/widgets/Quiz/views/quiz-single-select.php
+++ protected/components/widgets/Quiz/views/quiz-single-select.php
@@ -1,6 +1,6 @@
 <div class="quiz-single-select fluid-container">
     <div class="row">
-        <div class="col-xs-6 quiz-single-select__section">
+        <div class="col-xs-12 col-sm-12 col-md-6 quiz-single-select__section">
                 <label class="quiz-single-select__item">
                     <input type="radio" name="quiz-single-select" value="Репортажная"/>
                     Репортажная
@@ -15,7 +15,7 @@
                     Микс
                 </label>
         </div>
-        <div class="col-xs-6 quiz-single-select__section">
+        <div class="col-xs-12 col-sm-12 col-md-6 quiz-single-select__section">
              <img src="<?=Yii::app()->params['albumImagesWebDir']."4.jpg"?>"/>
         </div>
     </div>
