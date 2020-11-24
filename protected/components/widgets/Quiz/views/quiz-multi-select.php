84e844a466c42b48e98bb98322e8d66d1683c409
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-24 16:43:27 +0300

    Добавлена подсказка для multi select виджета

--- protected/components/widgets/Quiz/views/quiz-multi-select.php
+++ protected/components/widgets/Quiz/views/quiz-multi-select.php
@@ -1,4 +1,8 @@
 <div class="quiz-multi-select fluid-container">
+    <div class="alert alert-danger" role="alert">
+        <span class="mdi mdi-checkbox-marked-circle"></span>
+        Выберите один или несколько
+    </div>
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-6 quiz-multi-select__section">
             <?php foreach($options as $option): ?>
