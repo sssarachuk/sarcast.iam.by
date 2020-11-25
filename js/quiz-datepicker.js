49c94b309aebd14e6e6ef90159014cda67dea733
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-25 18:17:02 +0300

    Исправлен язык Datapicker представления

--- js/quiz-datepicker.js
+++ js/quiz-datepicker.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
 
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: 'Пред',
+        nextText: 'След',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+            'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+            'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        dayNames: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+        dayNamesShort: ['вс','пн','вт','ср','чт','пт','сб'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Нед',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''};
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+
     const datepicker = $('#quiz-datepicker').datepicker({
         showOtherMonths: true,
         selectOtherMonths: true,
         changeMonth: true,
         changeYear: true,
-        dateFormat: "dd/mm/yy",
         showOn: "button",
         buttonImage: "images/calendar-icon.png",
         buttonImageOnly: true,
--- protected/components/widgets/Quiz/views/quiz-range-selector.php
+++ protected/components/widgets/Quiz/views/quiz-range-selector.php
@@ -1,6 +1,6 @@
-<div class="quiz-single-select fluid-container">
+<div class="quiz-range-selector fluid-container">
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-6 quiz-single-select__section">
+        <div class="col-xs-12 col-sm-12 col-md-6 quiz-range-selector__section">
             Range Selector is under construction
         </div>
     </div>
