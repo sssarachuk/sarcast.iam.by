a893298b962bac2574b244d709d8c4349afac0b1
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-25 18:18:00 +0300

    Исправлен текст

--- protected/components/widgets/Quiz/views/quiz-datepicker.php
+++ protected/components/widgets/Quiz/views/quiz-datepicker.php
@@ -1,7 +1,7 @@
 <div class="quiz-datepicker fluid-container">
     <div class="alert alert-danger" role="alert">
         <span class="mdi mdi-checkbox-marked-circle"></span>
-        Используйте календарик для выбора даты
+        Используйте календарь для выбора даты
     </div>
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-6 quiz-datepicker__section">
