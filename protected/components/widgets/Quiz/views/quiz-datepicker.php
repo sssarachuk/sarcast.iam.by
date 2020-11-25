9a962e080f77331c6b17bf5e781f7a69e2701804
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-25 17:43:54 +0300

    Добавлена подсказка в datepicker

--- protected/components/widgets/Quiz/views/quiz-datepicker.php
+++ protected/components/widgets/Quiz/views/quiz-datepicker.php
@@ -1,4 +1,8 @@
 <div class="quiz-datepicker fluid-container">
+    <div class="alert alert-danger" role="alert">
+        <span class="mdi mdi-checkbox-marked-circle"></span>
+        Используйте календарик для выбора даты
+    </div>
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-6 quiz-datepicker__section">
             <input type="text"
