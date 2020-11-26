76795ffb00b9987dbea6616daaae47fbfd36735d
Alina Shpilevskaya <tonks.alina@gmail.com>
2020-11-26 18:28:23 +0300

    Немного изменений в стилях у DateRangeSelector

--- js/quiz-range-selector.js
+++ js/quiz-range-selector.js
@@ -9,9 +9,17 @@ $(document).ready(function(){
         min: slider.data().min,
         max: slider.data().max,
         values: [ fromInput.val(), toInput.val() ],
+        step: 5,
         slide: function( event, ui ) {
             fromInput.val(ui.values[0]);
             toInput.val(ui.values[1]);
         }
+    });
+
+    fromInput.on('change', function(e){
+        const element = this;
+        const fromValue = $(element).val();
+        const fromValueInt = Number(fromValue);
+        const step = $(element).data().step;
     });
 });
--- protected/components/widgets/Quiz/views/quiz-range-selector.php
+++ protected/components/widgets/Quiz/views/quiz-range-selector.php
@@ -7,14 +7,30 @@
         <div class="col-xs-12 col-sm-12 col-md-12 quiz-range-selector__section">
             <div class="row">
                 <div class="col-xs-6 col-sm-6 col-md-6 text-left">
-                    От <input type="text" id="quiz-range-selector-from" value="100">
+                    От <input type="text" id="quiz-range-selector-from"
+                              maxlength="4"
+                              pattern="\d*"
+                              value="100"
+                              data-step="5">
                 </div>
                 <div class="col-xs-6 col-sm-6 col-md-6 text-right">
-                    До <input type="text" id="quiz-range-selector-to" value="400">
+                    До <input type="text" id="quiz-range-selector-to"
+                              maxlength="4"
+                              pattern="\d*"
+                              value="400"
+                              data-step="5">
                 </div>
             </div>
             <div class="quiz-range-selector__slider-container">
                     <div id="quiz-range-selector-slider" data-min="0" data-max="500"></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6 text-left">
+                   <label>0</label>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 text-right">
+                    <label>500</label>
+                </div>
             </div>
         </div>
     </div>
