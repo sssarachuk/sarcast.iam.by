f5205410bb6a373d6e43dab6dfc444b05c1cd258
Siarhei <sssarachuk@gmail.com>
2022-07-21 18:18:31 +0300

    Изменения в пикселе фейсбука, и добавлены фото
    - обновлен пиксель фейсбука на новую верисию
    - уменьшен размер некоторых старых фотографий
    - заменена аватарка
    - добавлены 2 съемки

--- protected/views/layouts/layout_main.php
+++ protected/views/layouts/layout_main.php
@@ -44,6 +44,24 @@
    <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?154",t.onload=function(){VK.Retargeting.Init("VK-RTRG-249067-2MI7t"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-249067-2MI7t" style="position:fixed; left:-999px;" alt=""/></noscript>
 <!-- VK -->
 
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '181972618875634');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=181972618875634&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
 <!--End METRIKA-->
 
 <!--[if lt IE 10]>
